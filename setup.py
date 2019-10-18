83cf96f6dfa5496fa345a2396f0c00e09c9ab445
heronfonsaca <hfonsaca@gmail.com>
2019-10-18 12:25:51 -0300

    feat: update setup.py to version 0.0.2, remove pyathena dependency

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
     name="Lahap",
-    version="0.0.1",
-    packages=["lahap"],
-    url="",
+    version="0.0.2",
+    packages=find_packages(),
+    url="https://github.com/erathoslabs/lahap",
     license="MIT",
     author="Erathos",
     author_email="heron@erathos.com",
-    description="",
-    install_requires=["pyathena"],
+    description="Utility package to AWS Athena and AWS Glue.",
+    install_requires=["boto3"],
 )
