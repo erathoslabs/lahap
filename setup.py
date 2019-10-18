52fad0dfd4eb07899cc05d62e78307b396dec37c
heronfonsaca <hfonsaca@gmail.com>
2019-10-18 14:00:52 -0300

    feat: update project template files, add readme.md to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
 setup(
     name="Lahap",
     version="0.0.2",
@@ -11,5 +17,7 @@ setup(
     author="Erathos",
     author_email="heron@erathos.com",
     description="Utility package to AWS Athena and AWS Glue.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
     install_requires=["boto3"],
 )
