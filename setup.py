f25e699f2b8a02f0bd95f00603987c61058a2e3b
heronfonsaca <hfonsaca@gmail.com>
2019-10-24 18:19:08 -0300

    feat: bump version to 0.1

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
+import pathlib
 from distutils.core import setup
 
 from setuptools import find_packages
 
-
-def readme():
-    with open("README.md") as f:
-        return f.read()
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 
 setup(
     name="Lahap",
-    version="0.0.2",
+    version="0.1",
     packages=find_packages(),
     url="https://github.com/erathoslabs/lahap",
     license="MIT",
     author="Erathos",
     author_email="heron@erathos.com",
     description="Utility package to AWS Athena and AWS Glue.",
-    long_description=readme(),
+    long_description=README,
     long_description_content_type="text/markdown",
     install_requires=["boto3"],
 )
