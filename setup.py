a5bb7422976bb692b4976643d6d8960a7dd61c6d
heronfonsaca <hfonsaca@gmail.com>
2019-12-01 19:55:47 -0300

    bump to version 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="Lahap",
-    version="0.2",
+    version="0.2.1",
     packages=find_packages(),
     url="https://github.com/erathoslabs/lahap",
     license="MIT",
